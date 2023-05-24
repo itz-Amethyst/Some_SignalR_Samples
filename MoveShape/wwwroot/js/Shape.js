2ef29b2f13ff249bb631e74416cecc74980c4e59
itz-amethyst <pransermi@gmail.com>
2023-05-24 22:53:11 +0430

    feat: Added shape.js

---
+++ MoveShape/wwwroot/js/Shape.js
@@ -0,0 +1,16 @@
+﻿$shape = $("#shape");
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl('/shapeHub')
+    .build();
+
+connection.on('shapeMoved', function (x, y) {
+    $shape.css({ left: x, top: y });
+});
+
+connection.start().then(
+    $shape.draggable({
+        drag: function () {
+            connection.invoke("MoveShape", this.offsetLeft, this.offsetTop || 0);
+        }
+    })
+);
